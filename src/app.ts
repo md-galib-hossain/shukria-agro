7754e95451e89612f385afd5f30a5333c230f3dd
md-galib-hossain <mdgalib23@gmail.com>
2024-10-27 15:44:43 -0700

    Feat: not found middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import router from './app/routes'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/middlewares/notFound'
 const app : Application = express()
 
 //parsers
@@ -16,6 +17,6 @@ app.use("/api/v1",router)
 app.get('/',(req: Request, res: Response)=> {
 res.send('This is the root of Shukria Agro')
 })
-
+app.use(notFound)
 app.use(globalErrorHandler)
 export default app
